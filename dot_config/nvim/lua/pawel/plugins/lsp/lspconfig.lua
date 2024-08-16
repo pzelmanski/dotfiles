6be1e1551adbb04d15d050d1ad54669df8f2c4a1
pzelmanski <pawel.zelmanski.ext@timetoact.at>
2024-08-16 15:59:32 +0200

    some plugins changes

--- dot_config/nvim/lua/pawel/plugins/lsp/lspconfig.lua
+++ dot_config/nvim/lua/pawel/plugins/lsp/lspconfig.lua
@@ -90,7 +90,7 @@ return{
           capabilities = capabilities,
           on_attach = function(client, bufnr)
             vim.api.nvim_create_autocmd("BufWritePost", {
-              pattern = { "*.js", "*.ts" },
+              pattern = { "*.js", "*.ts", },
               callback = function(ctx)
                 -- Here use ctx.match instead of ctx.file
                 client.notify("$/onDidChangeTsOrJsFile", { uri = ctx.match })
